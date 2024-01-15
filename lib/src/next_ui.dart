0665e1756654d1f6ca7a0a93e896659c72517030
iamEtornam <sunumacbright@gmail.com>
2024-01-15 12:28:03 +0000

    generate project files

---
+++ lib/src/next_ui.dart
@@ -0,0 +1,7 @@
+/// {@template next_ui}
+/// Open source, Beautiful, modern and fast Flutter UI library.
+/// {@endtemplate}
+class NextUi {
+  /// {@macro next_ui}
+  const NextUi();
+}
---
+++ test/src/next_ui_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:next_ui/next_ui.dart';
+
+void main() {
+  group('NextUi', () {
+    test('can be instantiated', () {
+      expect(NextUi(), isNotNull);
+    });
+  });
+}
